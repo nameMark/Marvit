8a1cb41c6772833312de78a29ee2c1024614765f
nameMark <marko.dufala@gmail.com>
2020-03-04 14:41:03 +0100

    Second activity

--- app/src/main/java/com/example/maervit/MainActivity.java
+++ app/src/main/java/com/example/maervit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.maervit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -75,11 +76,23 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void handleResult(com.google.zxing.Result result) {
-            String resultCode = result.getText();
-            Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
+            String resultCodeString = result.getText();
+            long resultCode = Long.parseLong(resultCodeString);
+            long liner = 884851041913L;
+            if (resultCode == liner){
+                Toast.makeText(MainActivity.this, "prave si naskenoval liner", Toast.LENGTH_LONG).show();
+                openMainActivity2();
+            }else{
+                Toast.makeText(MainActivity.this, "neurcene", Toast.LENGTH_SHORT).show();
+            }
+
 
             setContentView(R.layout.activity_main);
             scannerView.stopCamera();
         }
+    }
+    public void openMainActivity2(){
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent );
     }
 }
